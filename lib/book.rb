4241e882035d95c996a225568ffa349941f04336
Bluesbaka <badunnells@gmail.com>
2019-05-16 16:17:17 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,35 @@
+require "pry"
+
+class Book
+
+attr_accessor :title, :author, :page_count, :genre
 
 
+def initialize(title)
+  @title = title
+  @author
+  @page_count
+  @genre
+end
+
+def title
+  @title
+end
+
+def author
+  @author
+end
+
+def genre
+  @genre
+end
+
+def page_count
+  @page_count
+end
+
+def turn_page
+    puts"Flipping the page...wow, you read fast!"
+end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,39 @@
-# Make your shoe class here!
+require "pry"
+class Shoe
+
+  attr_accessor :brand, :condition, :color, :material, :size
+
+def initialize(brand)
+    @brand = brand
+    @color
+    @condition
+    @material
+    @size
+end
+
+def brand
+  @brand
+end
+
+def condition
+  @condition
+end
+
+def material
+  @material
+end
+
+def color
+  @color
+end
+
+def size
+  @size
+end
+
+def cobble
+    puts"Your shoe is as good as new!"
+    @condition = "new"
+end
+
+end# Make your shoe class here!
